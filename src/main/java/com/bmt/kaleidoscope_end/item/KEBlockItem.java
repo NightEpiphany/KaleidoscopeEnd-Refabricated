29e7d4922f4e3dee05126ea433e5d164b5eae3cb
NightEpiphany <nightepiphany0401@gmail.com>
2026-05-04 21:42:16 +0800

    完善配方

--- src/main/java/com/bmt/kaleidoscope_end/item/KEBlockItem.java
+++ src/main/java/com/bmt/kaleidoscope_end/item/KEBlockItem.java
@@ -2,6 +2,7 @@ package com.bmt.kaleidoscope_end.item;
 
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.NotNull;
 
 public class KEBlockItem extends BlockItem {
     public KEBlockItem(Block block, Properties properties) {
@@ -9,7 +10,7 @@ public class KEBlockItem extends BlockItem {
     }
 
     @Override
-    public String getDescriptionId() {
+    public @NotNull String getDescriptionId() {
         return getOrCreateDescriptionId();
     }
 }
