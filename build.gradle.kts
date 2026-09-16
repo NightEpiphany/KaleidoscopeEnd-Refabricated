8eb9ce67f52b223cbec5022aad06efc606c8c571
NightEpiphany <nightepiphany0401@gmail.com>
2026-09-16 22:08:28 +0800

    名称修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,10 @@ plugins {
 version = providers.gradleProperty("mod_version").get()
 group = providers.gradleProperty("maven_group").get()
 
+base {
+	archivesName = providers.gradleProperty("archives_base_name")
+}
+
 repositories {
 	maven {
 		name = "Modrinth"
